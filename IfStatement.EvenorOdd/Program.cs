40c02887f38bce41077eca04ec0dd7f2766bb636
Mattea Swain <mmswain@tql.com>
2021-05-19 11:28:09 -0400

    Add project files.

---
+++ IfStatement.EvenorOdd/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IfStatement.EvenorOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var runAgain = true;
+            while (runAgain == true) 
+            { 
+            Console.Write("Enter an interger :");
+            var answerStir = Console.ReadLine();
+            var answer = int.Parse(answerStir);
+            var evenNumber = answer % 2 == 0;
+            if(evenNumber == true) 
+            {
+                Console.WriteLine($"{answer} is Even.");            
+            }
+            else 
+            {
+                Console.WriteLine($"{answer} is Odd.");
+            }
+            Console.Write("Continue? (Y if yes) :");
+                answerStir = Console.ReadLine();
+               if (answerStir == "y" || answerStir == "Y")
+                {
+                    runAgain = true;
+                } else
+                {
+                    runAgain = false;
+                }
+
+            }
+
+
+        }
+    }
+}
